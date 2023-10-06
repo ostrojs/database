614cba6db843e9e6969ddac22b80380eeb8a284c
Amar kumar singh <amar@ostrojs.com>
2023-10-07 00:23:22 +0530

    sqliteschema (#38)
    * Update databaseAdapter.js
    
    * Update builder.js
    
    * Update model.js
    
    * Update collection.js
    
    * Update interactsWithPivotTable.js
    
    * Update belongsTo.js
    
    * Update belongsToMany.js
    
    * Update hasOneThrough.js
    
    * Update hasManyThrough.js
    
    * Update hasOneOrMany.js
    
    * Update hasOneThrough.js
    
    * Update relation.js
    
    * Update guardsAttributes.js
    
    * Update hasGlobalScopes.js
    
    * Update hasRelationships.js
    
    * Update hasTimestamps.js
    
    * Update queriesRelationships.js
    
    * enhance model query and relation and added schema drop all table for oracle,mysql,pgsql and created adaptor for pgsql (#2)
    
    * merge changes (#29)
    
    * Update databaseAdapter.js
    
    * Update builder.js
    
    * Update model.js
    
    * Update collection.js
    
    * Update interactsWithPivotTable.js
    
    * Update belongsTo.js
    
    * Update belongsToMany.js
    
    * Update hasOneThrough.js
    
    * Update hasManyThrough.js
    
    * Update hasOneOrMany.js
    
    * Update hasOneThrough.js
    
    * Update relation.js
    
    * Update guardsAttributes.js
    
    * Update hasGlobalScopes.js
    
    * Update hasRelationships.js
    
    * Update hasTimestamps.js
    
    * Update queriesRelationships.js
    
    * Update sqlite.js
    
    * Create pgsql.js
    
    * Update statusCommand.js
    
    * Update seedCommand.js
    
    * Update wipeCommand.js
    
    * Update databaseAdapter.js
    
    * Update databaseManager.js
    
    * Update guardsAttributes.js
    
    * Update hasRelationships.js
    
    * Update model.js
    
    * Update databaseMigrationRepository.js
    
    * Update migrator.js
    
    * Update schema.js
    
    * Update mysqlSchema.js
    
    * Update sqliteSchema.js
    
    * Update oracleSchema.js
    
    * Create pgsql.js
    
    * Update and rename pgsql.js to pgsqlSchema.js
    
    * Update mysqlSchema.js
    
    * Update oracleSchema.js
    
    * Update pgsqlSchema.js
    
    * Update databaseManager.js
    
    * Update sqliteSchema.js

--- schema/sqliteSchema.js
+++ schema/sqliteSchema.js
@@ -1,15 +1,17 @@
-class Sqlite {
-    constructor(client, clientPackage = 'sqlite3', dbPath) {
-        return client({
-            client: clientPackage,
-            connection: {
-                filename: (dbPath)
-            },
-            useNullAsDefault: true,
-            asyncStackTraces: true
-        })
-    }
+const Schema = require('../schema')
+class SqliteSchema extends Schema {
+    static async dropAllTables(db) {
+        const promises = [];
+        const tableNames = await db.table('sqlite_master')
+            .where('type', db.getConfig('schema', 'table'))
+            .whereNot('name', 'like', 'sqlite_%') // Exclude system tables
+            .pluck('name');
+        for (const name of tableNames) {
+            promises.push(this.dropIfExists(name));
+        }
+        return Promise.all(promises)
 
+    }
 }
 
-module.exports = Sqlite
+module.exports = SqliteSchema
