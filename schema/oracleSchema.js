4050d38b637e309fa1049f612fe8613954d1928c
Amar kumar singh <amar@ostrojs.com>
2023-10-07 00:17:53 +0530

    Update oracleSchema.js

--- schema/oracleSchema.js
+++ schema/oracleSchema.js
@@ -1,13 +1,15 @@
-class Oracle {
-    constructor(client, connection) {
-        return client({
-            client: 'oracle',
-            connection: connection,
-            useNullAsDefault: true,
-            asyncStackTraces: true
-        })
-    }
+const Schema = require('../schema')
+class OracleSchema extends Schema {
+    static async dropAllTables(db) {
+        const promises = [];
+        const tableNames = await db.table('user_tables')
+            .pluck('table_name');
+        for (const name of tableNames) {
+            promises.push(this.dropIfExists(name));
+        }
+        return Promise.all(promises)
 
+    }
 }
 
-module.exports = Oracle
+module.exports = OracleSchema
