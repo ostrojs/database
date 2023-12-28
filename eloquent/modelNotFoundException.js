dae3fa07c80e9ce8f9db6f95ed81dfc0fd1399e2
Amar kumar singh <amar@ostrojs.com>
2023-12-28 14:50:48 +0530

    bug fix for serModel

--- eloquent/modelNotFoundException.js
+++ eloquent/modelNotFoundException.js
@@ -1,26 +1,28 @@
 const { get_class_name } = require("@ostro/support/function");
+const ModelNotFoundExceptionContracts = require("@ostro/contracts/database/eloquent/modelNotFoundException");
 
-class ModelNotFoundException extends Error {
+class ModelNotFoundException extends ModelNotFoundExceptionContracts {
     $model;
 
     $ids;
 
     constructor() {
+        super();
         this.name = this.constructor.name;
         this.code = 'ERR_INVALID_ARG_VALUE';
         this.statusCode = 500
-        Error.captureStackTrace(this, this.constructor);
+
     }
 
 
     setModel($model, $ids = []) {
         this.$model = $model;
-        this.$ids = $ids.wrap();
+        this.$ids = $ids;
 
         this.message = `No query results for model [${get_class_name($model)}]`;
 
-        if (count(this.ids) > 0) {
-            this.message += ' ' + this.ids.join(', ');
+        if (count(this.$ids) > 0) {
+            this.message += ' ' + this.$ids.join(', ');
         } else {
             this.message += '.';
         }
